ff8636d8f067dcf58751c0eeffc81875fd6ae4f3
ilyas <alaouiilyas44@gmail.com>
2024-04-19 09:47:47 +0100

    start angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './Components/movies/movies.component';
+import { MovieComponent } from './Components/movie/movie.component';
+import { LoginComponent } from './Components/login/login.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    MovieComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
